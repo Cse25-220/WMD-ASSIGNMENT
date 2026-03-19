521a1388252643a893abd00b3d1d7d4dea98f7d7
Boagoe <cse25-220@thuto.bac.ac.bw>
2026-03-19 16:44:50 +0200

    add java script

---
+++ store.js
@@ -0,0 +1,13 @@
+const products = [
+  // SHIRTS
+  {
+    id: 1,
+    category: 'shirts',
+    brand: 'Nike',
+    name: 'Manchester City Home Shirt 2024/25',
+    price: 79.99,
+    oldPrice: null,
+    badge: 'new',
+    badgeText: 'NEW',
+    img: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/1b9c8e5a1c7b4e5d9f0eaa000d2c8c3_9366/Manchester_City_Home_Jersey_Blue_GKQ86.jpg',
+  },
