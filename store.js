fc84be59d156cf761d41d73648a82f179327bf0e
Boagoe <cse25-220@thuto.bac.ac.bw>
2026-03-19 16:46:06 +0200

    add Manchester City Home Jersey to store.js

--- store.js
+++ store.js
@@ -10,4 +10,4 @@ const products = [
     badge: 'new',
     badgeText: 'NEW',
     img: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/1b9c8e5a1c7b4e5d9f0eaa000d2c8c3_9366/Manchester_City_Home_Jersey_Blue_GKQ86.jpg',
-  },
+  }
