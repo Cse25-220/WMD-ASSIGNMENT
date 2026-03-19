37e95b6f53da26cb011bb2d05a1d12bdccf6d247
Boagoe <cse25-220@thuto.bac.ac.bw>
2026-03-19 16:50:15 +0200

    add liverpool shirt

--- store.js
+++ store.js
@@ -5,9 +5,21 @@ const products = [
     category: 'shirts',
     brand: 'Nike',
     name: 'Manchester City Home Shirt 2024/25',
-    price: 79.99,
+    price: 1000.00,
     oldPrice: null,
     badge: 'new',
     badgeText: 'NEW',
     img: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/1b9c8e5a1c7b4e5d9f0eaa000d2c8c3_9366/Manchester_City_Home_Jersey_Blue_GKQ86.jpg',
+  },
+{
+    id: 2,
+    category: 'shirts',
+    brand: 'Adidas',
+    name: 'liverpool Home Shirt 2025/26',
+    price: 979.99,
+    oldPrice: 1200.00,
+    badge: 'new',
+    badgeText: 'NEW',
+    img: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/1b9c8e5a1c7b4e5d9f0eaa000d2c8c3_9366/Manchester_City_Home_Jersey_Blue_GKQ86.jpg',
   }
+]
